728f518d245c2e1eed745f89497cb225540ce1aa
ferreirahugo2-hue <ferreira.hugo2@aluno.ifsp.edu.br>
2026-06-21 22:42:09 -0300

    Aula 12, ex 2c

---
+++ Aula 12, ex 2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char nome[100];
+    
+    printf("Digite seu primeiro nome: ");
+    scanf("%s", nome);
+
+    // Converter para minúsculas
+    for (int i = 0; nome[i] != '\0'; i++) {
+        nome[i] = tolower(nome[i]);
+    }
+
+    int tamanho = strlen(nome);
+
+    printf("Quantidade de letras: %d\n", tamanho);
+
+    // Imprimir nome invertido
+    printf("Nome invertido: ");
+    for (int i = tamanho - 1; i >= 0; i--) {
+        printf("%c", nome[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
